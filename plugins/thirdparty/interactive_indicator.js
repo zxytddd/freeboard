360029ff2469e9ce351ab7183915ce828d56be50
Huaqi Fang <Huaqi.Fang@synopsys.com>
2016-04-01 16:34:34 +0800

    Format third parties plugins

--- plugins/thirdparty/interactive_indicator.js
+++ plugins/thirdparty/interactive_indicator.js
@@ -1,45 +1,39 @@
-(function()
-{
+(function() {
 
     freeboard.loadWidgetPlugin({
         type_name: "interactive_indicator",
         display_name: "Interactive Indicator Light",
-        description : "Indicator which can send a value as well as recieve one",
-        settings: [
-            {
+        description: "Indicator which can send a value as well as receive one",
+        settings: [{
                 name: "title",
                 display_name: "Title",
                 type: "text"
-            },
-            {
+            }, {
                 name: "value",
                 display_name: "Value",
                 type: "calculated"
-            },
-            {
+            }, {
                 name: "callback",
                 display_name: "Datasource to send to",
                 type: "calculated"
-            },
-            {
+            }, {
                 name: "on_text",
                 display_name: "On Text",
                 type: "calculated"
-            },
-            {
+            }, {
                 name: "off_text",
                 display_name: "Off Text",
                 type: "calculated"
             },
 
         ],
-        newInstance: function (settings, newInstanceCallback) {
+        newInstance: function(settings, newInstanceCallback) {
             newInstanceCallback(new interactiveIndicator(settings));
         }
     });
 
     freeboard.addStyle('.indicator-light.interactive:hover', "box-shadow: 0px 0px 15px #FF9900; cursor: pointer;");
-    var interactiveIndicator = function (settings) {
+    var interactiveIndicator = function(settings) {
         var self = this;
         var titleElement = $('<h2 class="section-title"></h2>');
         var stateElement = $('<div class="indicator-text"></div>');
@@ -54,8 +48,7 @@
 
             if (isOn) {
                 stateElement.text((_.isUndefined(onText) ? (_.isUndefined(currentSettings.on_text) ? "" : currentSettings.on_text) : onText));
-            }
-            else {
+            } else {
                 stateElement.text((_.isUndefined(offText) ? (_.isUndefined(currentSettings.off_text) ? "" : currentSettings.off_text) : offText));
             }
         }
@@ -72,18 +65,18 @@
         }
 
 
-        this.render = function (element) {
+        this.render = function(element) {
             $(element).append(titleElement).append(indicatorElement).append(stateElement);
             $(indicatorElement).click(this.onClick.bind(this));
         }
 
-        this.onSettingsChanged = function (newSettings) {
+        this.onSettingsChanged = function(newSettings) {
             currentSettings = newSettings;
             titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
             updateState();
         }
 
-        this.onCalculatedValueChanged = function (settingName, newValue) {
+        this.onCalculatedValueChanged = function(settingName, newValue) {
             if (settingName == "value") {
                 isOn = Boolean(newValue);
             }
@@ -97,10 +90,9 @@
             updateState();
         }
 
-        this.onDispose = function () {
-        }
+        this.onDispose = function() {}
 
-        this.getHeight = function () {
+        this.getHeight = function() {
             return 1;
         }
 
