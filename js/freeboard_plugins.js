91fc95e29a3955e8878aae00c633518b63065f16
Xinyi Zhao <xinyi@synopsys.com>
2016-08-22 16:54:19 +0800

    Delete the useless plugin.

--- js/freeboard_plugins.js
+++ js/freeboard_plugins.js
@@ -3191,79 +3191,7 @@ $.extend(freeboard, jQuery.eventEmitter);
 		}
 	};
 
-	freeboard.loadDatasourcePlugin({
-		type_name: "JSON",
-		settings: [
-			{
-				name: "url",
-				display_name: "URL",
-				type: "text"
-			},
-			{
-				name: "use_thingproxy",
-				display_name: "Try thingproxy",
-				description: 'A direct JSON connection will be tried first, if that fails, a JSONP connection will be tried. If that fails, you can use thingproxy, which can solve many connection problems to APIs. <a href="https://github.com/Freeboard/thingproxy" target="_blank">More information</a>.',
-				type: "boolean",
-				default_value: true
-			},
-			{
-				name: "refresh",
-				display_name: "Refresh Every",
-				type: "number",
-				suffix: "seconds",
-				default_value: 5
-			},
-			{
-				name: "method",
-				display_name: "Method",
-				type: "option",
-				options: [
-					{
-						name: "GET",
-						value: "GET"
-					},
-					{
-						name: "POST",
-						value: "POST"
-					},
-					{
-						name: "PUT",
-						value: "PUT"
-					},
-					{
-						name: "DELETE",
-						value: "DELETE"
-					}
-				]
-			},
-			{
-				name: "body",
-				display_name: "Body",
-				type: "text",
-				description: "The body of the request. Normally only used if method is POST"
-			},
-			{
-				name: "headers",
-				display_name: "Headers",
-				type: "array",
-				settings: [
-					{
-						name: "name",
-						display_name: "Name",
-						type: "text"
-					},
-					{
-						name: "value",
-						display_name: "Value",
-						type: "text"
-					}
-				]
-			}
-		],
-		newInstance: function (settings, newInstanceCallback, updateCallback) {
-			newInstanceCallback(new jsonDatasource(settings, updateCallback));
-		}
-	});
+	
 
 	var openWeatherMapDatasource = function (settings, updateCallback) {
 		var self = this;
@@ -3327,50 +3255,6 @@ $.extend(freeboard, jQuery.eventEmitter);
 		}
 	};
 
-	freeboard.loadDatasourcePlugin({
-		type_name: "openweathermap",
-		display_name: "Open Weather Map API",
-		settings: [
-			{
-				name: "api_key",
-				display_name: "API Key",
-				type: "text",
-				description: "Your personal API Key from Open Weather Map"
-			},
-            {
-				name: "location",
-				display_name: "Location",
-				type: "text",
-				description: "Example: London, UK"
-			},
-			{
-				name: "units",
-				display_name: "Units",
-				type: "option",
-				default: "imperial",
-				options: [
-					{
-						name: "Imperial",
-						value: "imperial"
-					},
-					{
-						name: "Metric",
-						value: "metric"
-					}
-				]
-			},
-			{
-				name: "refresh",
-				display_name: "Refresh Every",
-				type: "number",
-				suffix: "seconds",
-				default_value: 5
-			}
-		],
-		newInstance: function (settings, newInstanceCallback, updateCallback) {
-			newInstanceCallback(new openWeatherMapDatasource(settings, updateCallback));
-		}
-	});
 
 	var dweetioDatasource = function (settings, updateCallback) {
 		var self = this;
@@ -3407,25 +3291,6 @@ $.extend(freeboard, jQuery.eventEmitter);
 
 		self.onSettingsChanged(settings);
 	};
-
-	freeboard.loadDatasourcePlugin({
-		"type_name": "dweet_io",
-		"display_name": "Dweet.io",
-		"external_scripts": [
-			"http://dweet.io/client/dweet.io.min.js"
-		],
-		"settings": [
-			{
-				name: "thing_id",
-				display_name: "Thing Name",
-				"description": "Example: salty-dog-1",
-				type: "text"
-			}
-		],
-		newInstance: function (settings, newInstanceCallback, updateCallback) {
-			newInstanceCallback(new dweetioDatasource(settings, updateCallback));
-		}
-	});
 
 	var playbackDatasource = function (settings, updateCallback) {
 		var self = this;
@@ -3497,40 +3362,6 @@ $.extend(freeboard, jQuery.eventEmitter);
 		}
 	};
 
-	freeboard.loadDatasourcePlugin({
-		"type_name": "playback",
-		"display_name": "Playback",
-		"settings": [
-			{
-				"name": "datafile",
-				"display_name": "Data File URL",
-				"type": "text",
-				"description": "A link to a JSON array of data."
-			},
-			{
-				name: "is_jsonp",
-				display_name: "Is JSONP",
-				type: "boolean"
-			},
-			{
-				"name": "loop",
-				"display_name": "Loop",
-				"type": "boolean",
-				"description": "Rewind and loop when finished"
-			},
-			{
-				"name": "refresh",
-				"display_name": "Refresh Every",
-				"type": "number",
-				"suffix": "seconds",
-				"default_value": 5
-			}
-		],
-		newInstance: function (settings, newInstanceCallback, updateCallback) {
-			newInstanceCallback(new playbackDatasource(settings, updateCallback));
-		}
-	});
-
 	var clockDatasource = function (settings, updateCallback) {
 		var self = this;
 		var currentSettings = settings;
@@ -3574,103 +3405,7 @@ $.extend(freeboard, jQuery.eventEmitter);
 		updateTimer();
 	};
 
-	freeboard.loadDatasourcePlugin({
-		"type_name": "clock",
-		"display_name": "Clock",
-		"settings": [
-			{
-				"name": "refresh",
-				"display_name": "Refresh Every",
-				"type": "number",
-				"suffix": "seconds",
-				"default_value": 1
-			}
-		],
-		newInstance: function (settings, newInstanceCallback, updateCallback) {
-			newInstanceCallback(new clockDatasource(settings, updateCallback));
-		}
-	});
-freeboard.loadDatasourcePlugin({
-		// **type_name** (required) : A unique name for this plugin. This name should be as unique as possible to avoid collisions with other plugins, and should follow naming conventions for javascript variable and function declarations.
-		"type_name"   : "meshblu",
-		// **display_name** : The pretty name that will be used for display purposes for this plugin. If the name is not defined, type_name will be used instead.
-		"display_name": "Octoblu",
-        // **description** : A description of the plugin. This description will be displayed when the plugin is selected or within search results (in the future). The description may contain HTML if needed.
-        "description" : "app.octoblu.com",
-		// **external_scripts** : Any external scripts that should be loaded before the plugin instance is created.
-		"external_scripts" : [
-			"http://meshblu.octoblu.com/js/meshblu.js"
-		],
-		// **settings** : An array of settings that will be displayed for this plugin when the user adds it.
-		"settings"    : [
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "uuid",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "UUID",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "device uuid",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your device UUID",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			},
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "token",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Token",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "device token",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your device TOKEN",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			},
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "server",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Server",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "meshblu.octoblu.com",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your server",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			},
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "port",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Port",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "number",
-				// **default_value** : A default value for this setting.
-				"default_value": 80,
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "server port",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			}
-			
-		],
-		// **newInstance(settings, newInstanceCallback, updateCallback)** (required) : A function that will be called when a new instance of this plugin is requested.
-		// * **settings** : A javascript object with the initial settings set by the user. The names of the properties in the object will correspond to the setting names defined above.
-		// * **newInstanceCallback** : A callback function that you'll call when the new instance of the plugin is ready. This function expects a single argument, which is the new instance of your plugin object.
-		// * **updateCallback** : A callback function that you'll call if and when your datasource has an update for freeboard to recalculate. This function expects a single parameter which is a javascript object with the new, updated data. You should hold on to this reference and call it when needed.
-		newInstance   : function(settings, newInstanceCallback, updateCallback)
-		{
-			// myDatasourcePlugin is defined below.
-			newInstanceCallback(new meshbluSource(settings, updateCallback));
-		}
-	});
+
 
 
 	// ### Datasource Implementation
